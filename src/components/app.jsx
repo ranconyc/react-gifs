f72dd59368cf88a13e17a9bc9a1f5a53b13b4977
ran cohen <ranco.nyc@gmail.com>
2020-08-28 14:07:40 -0400

    giphy app

---
+++ src/components/app.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "hQLXtj9RnHuguqwzWN" 
+    };
+  }
+  
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  search = (query) => {
+    giphy('wQO4fglXzVxiqGfiKkDlOKCzmEq477CB').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='left-scene'>
+          <SearchBar searchFunction={this.search} />
+          <div className='selected-gif'> 
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className='right-scene'>
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App; 
+  
